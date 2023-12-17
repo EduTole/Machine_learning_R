9ce1fefcc6b0e25df3834fa58dc6a7234986b62d
Edinson Tolentino <73961653+EduTole@users.noreply.github.com>
2023-12-17 19:52:39 +0000

    Add files via upload

---
+++ CV/code/ml_support_function.R
@@ -0,0 +1,12 @@
+
+# Function
+#--------------------------------------------
+
+# Machine learning function
+# 1. function across RMSE
+twoClassSummaryExtended <- function (data, lev = NULL, model = NULL)
+{
+  lvls <- levels(data$obs)
+  rmse <- sqrt(mean((data[, lvls[1]] - ifelse(data$obs == lev[2], 0, 1))^2))
+  c(defaultSummary(data, lev, model), "RMSE" = rmse)
+}
